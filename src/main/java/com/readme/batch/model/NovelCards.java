8a9d68420fd6c4ad1a556cd27ca7e7d660a87e9c
jinyeong-afk <xoeh7002@naver.com>
2023-05-23 11:50:27 +0900

    [CREATE] #BTS-139 : mongoDB NovelCards 연결

---
+++ src/main/java/com/readme/batch/model/NovelCards.java
@@ -0,0 +1,47 @@
+package com.readme.batch.model;
+
+import java.util.Date;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "novel_cards")
+@Getter
+@NoArgsConstructor
+public class NovelCards {
+
+    @Id
+    private Long novelId;
+    @Indexed
+    private String title;
+    private String description;
+    private String author;
+    private String authorComment;
+    private String genre;
+    private Integer grade;
+    private String thumbnail;
+    private Date startDate;
+    private Long views;
+    private String serializationStatus;
+    private List<Tag> tags;
+    private Long scheduleId;
+    private Double starRating;
+    private Boolean monday;
+    private Boolean tuesday;
+    private Boolean wednesday;
+    private Boolean thursday;
+    private Boolean friday;
+    private Boolean saturday;
+    private Boolean sunday;
+    private Long episodeCount;
+
+    @Getter
+    public static class Tag {
+
+        private Long id;
+        private String name;
+    }
+}
