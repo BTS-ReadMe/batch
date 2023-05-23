c932be8d49745d40114e65c74f54c4e343891053
jinyeong-afk <xoeh7002@naver.com>
2023-05-23 10:50:05 +0900

    [CREATE] : Project Config

---
+++ src/test/java/com/readme/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readme.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
