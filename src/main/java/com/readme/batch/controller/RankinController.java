b1d413e7f93d1f3fabebbe6234f53646c4c83d99
jinyeong-afk <xoeh7002@naver.com>
2023-06-01 22:45:45 +0900

    [UPDATE] #BTS-155 : CommonDataResponse에 담아서 보내도록 변경

--- src/main/java/com/readme/batch/controller/RankinController.java
+++ src/main/java/com/readme/batch/controller/RankinController.java
@@ -1,5 +1,6 @@
 package com.readme.batch.controller;
 
+import com.readme.batch.commonResponseObject.CommonDataResponse;
 import com.readme.batch.model.NovelViews;
 import com.readme.batch.repository.NovelViewsRepository;
 import com.readme.batch.responseObject.ResponseRanking;
@@ -7,6 +8,7 @@ import com.readme.batch.service.NovelViewsService;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RankinController {
     private final NovelViewsService novelViewsService;
     @GetMapping
-    public ResponseRanking getRanking() {
-        return novelViewsService.getRanking();
+    public ResponseEntity<CommonDataResponse<ResponseRanking>> getRanking() {
+        return ResponseEntity.ok(new CommonDataResponse(novelViewsService.getRanking()));
     }
 }
