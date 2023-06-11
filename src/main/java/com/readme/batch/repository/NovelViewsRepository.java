f70ad8f448baefabbdbeb189cf11cc76a20e0b38
jinyeong-afk <xoeh7002@naver.com>
2023-06-11 20:23:10 +0900

    [UPDATE] #BTS-172 : Ranking 조회 시 main_category join 하여 genre 가져오도록 변경

--- src/main/java/com/readme/batch/repository/NovelViewsRepository.java
+++ src/main/java/com/readme/batch/repository/NovelViewsRepository.java
@@ -13,10 +13,12 @@ import org.springframework.stereotype.Repository;
 public interface NovelViewsRepository extends JpaRepository<NovelViews, Long> {
     Boolean existsByNovelIdAndViewsDate(Long novelId, LocalDateTime viewsDate);
     NovelViews findByNovelIdAndViewsDate(Long novelId, LocalDateTime viewsDate);
+
     @Query(nativeQuery = true, value =
-        "SELECT novels.id, novels.author, novels.genre, novels.grade, novels.thumbnail, novels.serialization_status, novels.title, novel_views.views_date, RANK() OVER (ORDER BY novel_views.views DESC) ranking " +
+        "SELECT novels.id, novels.author, main_category.title as genre, novels.grade, novels.thumbnail, novels.serialization_status, novels.title, novel_views.views_date, ROW_NUMBER() OVER (ORDER BY novel_views.views DESC) ranking " +
             "FROM novels " +
             "INNER JOIN novel_views ON novels.id = novel_views.novel_id " +
+            "INNER JOIN main_category ON novels.genre = main_category.id " +
             "WHERE novel_views.views_date = :viewsDate " +
             "LIMIT 15")
     List<INovelViews> findAllByViewsDateWithRank(@Param("viewsDate") LocalDateTime viewsDate);
