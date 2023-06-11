f2ea9f2e3be2181b8fbf53335c2fdf9894ea7c00
Ju-Yeongmin <drodudals1@gmail.com>
2023-06-11 13:33:25 +0900

    [UPDATE] batch prometheus 추가
    - batch service 에 prometheus dependency 및 api 추가

---
+++ src/main/java/com/readme/batch/config/AspectConfig.java
@@ -0,0 +1,9 @@
+package com.readme.batch.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfig {
+}
